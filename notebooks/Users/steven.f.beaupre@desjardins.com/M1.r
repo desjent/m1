e040a3f5a9101bbf64c04bad82346482b2b4b561
desjent <40123708+desjent@users.noreply.github.com>
2018-06-10 10:12:59 -0400

    Trying stuff

--- notebooks/Users/steven.f.beaupre@desjardins.com/M1.r
+++ notebooks/Users/steven.f.beaupre@desjardins.com/M1.r
@@ -1,1 +1,30 @@
 # Databricks notebook source
+library(tidyverse)
+library(sparklyr)
+
+sc <- spark_connect(method = "databricks")
+mtcars_tbl <- sdf_copy_to(sc, mtcars, "mtcars", overwrite = TRUE)
+
+partitions <- mtcars_tbl %>%
+  filter(hp >= 100) %>%
+  sdf_mutate(cyl8 = ft_bucketizer(cyl, c(0,8,12))) %>%
+  sdf_partition(training = 0.5, test = 0.5, seed = 888)
+
+# fit a linear mdoel to the training dataset
+fit <- partitions$training %>%
+  ml_linear_regression(mpg ~ wt + cyl)
+
+pred <- sdf_predict(fit, partitions$test) %>%
+  collect
+
+# Plot the predicted versus actual mpg
+ggplot(pred, aes(x = mpg, y = prediction)) +
+  geom_abline(lty = "dashed", col = "red") +
+  geom_point() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  coord_fixed(ratio = 1) +
+  labs(
+    x = "Actual Fuel Consumption",
+    y = "Predicted Fuel Consumption",
+    title = "Predicted vs. Actual Fuel Consumption"
+  )
